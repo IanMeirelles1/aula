a79ac322cd18ab2e12075c1bd65baf95c6d3e60b
IanMeirelles1 <160203412+IanMeirelles1@users.noreply.github.com>
2024-03-19 17:23:46 +0000

    Tue, Mar 19, 2024, 2:23 PM -03:00

--- airport.py
+++ airport.py
@@ -3,6 +3,8 @@ x2 = 10
 x3 = 10
 x4 = 10
 x5 = 10
+
+
 def main():
     while True:
         print("Voos disponíveis: AS7012, QX2002, AS2002, 8E880, 8E890")
@@ -17,23 +19,29 @@ def main():
             sis(x4)
         elif voo == "8E890":
             sis(x5)
-            
+        else:
+            print("Voo inválido")
+
+
 def sis(x):
     print(f"Este voo possui {x} passagens")
-        quant = int(input("Quantidade de passagens: "))
-        x -= quant
-        if x < 0:
-            print("Passagens insuficientes")
-            x += quant
+    quant = int(input("Quantidade de passagens: "))
+    x -= quant
+    if x < 0:
+        print("Passagens insuficientes")
+        x += quant
+    else:
+        if x <= 5:
+            valor = (quant * 100) + (quant * 100) * 0.20
+            print(f"O valor a ser pago é de R${valor}")
         else:
-            if x <= 5:
-                valor = (quant * 100) + (quant * 100) * 0.20
-                print(f"O valor a ser pago é de R${valor}")
-            else:
-                valor = quant * 100
-                print(f"O valor a ser pago é de R${valor}")
-            cont = input("Quer continuar sua compra? ")
-            if cont == "Sim":
-                continue
-            else:
-                break
+            valor = quant * 100
+            print(f"O valor a ser pago é de R${valor}")
+        cont = input("Quer continuar sua compra? ")
+        if cont == "Sim":
+            continue
+        else:
+            break
+
+
+main()
