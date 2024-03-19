2b1425821dea934915eb3d636930c90fb49c11bc
IanMeirelles1 <160203412+IanMeirelles1@users.noreply.github.com>
2024-03-19 17:12:27 +0000

    Tue, Mar 19, 2024, 2:12 PM -03:00

---
+++ card.py
@@ -0,0 +1,15 @@
+def main():
+    x = input("x: ")
+    x1 = input("x1: ")
+    ian(x, x1)
+
+
+def ian(a, b):
+    if a == "1":
+        print("tt")
+    else:
+        b = b.upper
+        print(b)
+main()
+
+
