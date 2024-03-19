ff216437a0a9843f25367102ddb959cba324c767
IanMeirelles1 <160203412+IanMeirelles1@users.noreply.github.com>
2024-03-19 17:19:46 +0000

    Tue, Mar 19, 2024, 2:19 PM -03:00

--- airport.py
+++ airport.py
@@ -8,25 +8,7 @@ def main():
         print("Voos disponíveis: AS7012, QX2002, AS2002, 8E880, 8E890")
         voo = input("Qual voo a ser consultado: ")
         if voo == "AS7012":
-            print(f"Este voo possui {x1} passagens")
-        quant = int(input("Quantidade de passagens: "))
-        x1 -= quant
-        if x1 < 0:
-            print("Passagens insuficientes")
-            x1 += quant
-        else:
-            if x1 <= 5:
-                valor = (quant * 100) + (quant * 100) * 0.20
-                print(f"O valor a ser pago é de R${valor}")
-            else:
-                valor = quant * 100
-                print(f"O valor a ser pago é de R${valor}")
-            cont = input("Quer continuar sua compra? ")
-            if cont == "Sim":
-                continue
-            else:
-                break
-
+            sis(x1)
 def sis(x):
     print(f"Este voo possui {x} passagens")
         quant = int(input("Quantidade de passagens: "))
