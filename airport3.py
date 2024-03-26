7e80865d5cad0b4bf243db74b3ffc10e2b7dd402
IanMeirelles1 <160203412+IanMeirelles1@users.noreply.github.com>
2024-03-26 18:46:18 +0000

    Tue, Mar 26, 2024, 3:46 PM -03:00

--- airport3.py
+++ airport3.py
@@ -14,19 +14,19 @@ def main():
             voo = input("Qual voo a ser consultado: ")
 
             if voo == "AS7012":
-                valor1 = sis(voos["AS7012"][0], voos["AS7012"][1], voos["AS7012"][2], "AS7012")
+                valor1 += sis(voos["AS7012"][0], voos["AS7012"][1], voos["AS7012"][2], "AS7012")
 
             elif voo == "QX2002":
-                valor2 = sis(voos["QX2002"][0], voos["QX2002"][1], voos["QX2002"][2], "QX2002")
+                valor2 += sis(voos["QX2002"][0], voos["QX2002"][1], voos["QX2002"][2], "QX2002")
 
             elif voo == "AS2002":
-                valor3 = sis(voos["AS2002"][0], voos["AS2002"][1], voos["AS2002"][2], "AS2002")
+                valor3 += sis(voos["AS2002"][0], voos["AS2002"][1], voos["AS2002"][2], "AS2002")
 
             elif voo == "8E880":
-                valor4 = sis(voos["8E880"][0], voos["8E880"][1], voos["8E880"][2], "8E880")
+                valor4 += sis(voos["8E880"][0], voos["8E880"][1], voos["8E880"][2], "8E880")
 
             elif voo == "8E890":
-                valor5 = sis(voos["8E890"][0], voos["8E890"][1], voos["8E890"][2], "8E890")
+                valor5 += sis(voos["8E890"][0], voos["8E890"][1], voos["8E890"][2], "8E890")
 
             perg = input("Deseja continuar a compra: ")
             if perg == "Sim":
